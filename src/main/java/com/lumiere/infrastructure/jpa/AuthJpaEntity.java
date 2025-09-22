8d3211c4887bae6ec4da34037af1f6b756764ba9
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-09-22 15:52:41 -0300

    feat: auth jpa entity

---
+++ src/main/java/com/lumiere/infrastructure/jpa/AuthJpaEntity.java
@@ -0,0 +1,76 @@
+package com.lumiere.infrastructure.jpa;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
+import java.util.UUID;
+
+@Entity(name = "Auth")
+public class AuthJpaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @OneToOne(mappedBy = "auth", cascade = CascadeType.ALL, orphanRemoval = true)
+    private UserJpaEntity user;
+
+    private String name;
+    private String email;
+    private String password;
+    private Boolean isAdmin;
+
+    // Getters
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Boolean getIsAdmin() {
+        return isAdmin;
+    }
+
+    public UserJpaEntity getUser() {
+        return user;
+    }
+
+    // Setters
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setisAdmin(boolean isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+
+    public void setUser(UserJpaEntity user) {
+        this.user = user;
+    }
+}
