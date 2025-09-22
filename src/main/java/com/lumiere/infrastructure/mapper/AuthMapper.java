7910fc90d15ffa1140266e18bb8a2a88c3456d9f
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-09-22 17:07:34 -0300

    feat: auth mapper

---
+++ src/main/java/com/lumiere/infrastructure/mapper/AuthMapper.java
@@ -0,0 +1,28 @@
+package com.lumiere.infrastructure.mapper;
+
+import com.lumiere.domain.entity.Auth;
+import com.lumiere.infrastructure.jpa.AuthJpaEntity;
+
+public class AuthMapper {
+
+    // JPA → Domain
+    public static Auth toDomain(AuthJpaEntity jpaEntity) {
+        return new Auth(
+                jpaEntity.getId(),
+                jpaEntity.getName(),
+                jpaEntity.getEmail(),
+                jpaEntity.getPassword(),
+                jpaEntity.getIsAdmin());
+    }
+
+    // Domain → JPA
+    public static AuthJpaEntity toJpa(Auth domain) {
+        AuthJpaEntity jpa = new AuthJpaEntity();
+        jpa.setId(domain.getId());
+        jpa.setName(domain.getName());
+        jpa.setEmail(domain.getEmail());
+        jpa.setPassword(domain.getPassword());
+        jpa.setisAdmin(domain.getisAdmin());
+        return jpa;
+    }
+}
