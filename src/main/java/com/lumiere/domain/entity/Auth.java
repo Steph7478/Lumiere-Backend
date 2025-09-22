5ae802eb44c53f66c3d9a456f7465e89fb60b90e
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-09-22 10:27:52 -0300

    feat: isAdmin in Auth Entity

--- src/main/java/com/lumiere/domain/entity/Auth.java
+++ src/main/java/com/lumiere/domain/entity/Auth.java
@@ -8,14 +8,17 @@ public class Auth {
     private String name;
     private String email;
     private String password;
+    private boolean isAdmin;
 
-    public Auth(UUID id, String name, String email, String password) {
+    public Auth(UUID id, String name, String email, String password, Boolean isAdmin) {
         this.id = id != null ? id : UUID.randomUUID();
         this.name = name;
         this.email = email;
         this.password = password;
+        this.isAdmin = isAdmin;
     }
 
+    // Getters
     public UUID getId() {
         return id;
     }
@@ -24,20 +27,29 @@ public class Auth {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public Boolean getisAdmin() {
+        return isAdmin;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    // Setters
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public String getPassword() {
-        return password;
+    public void setisAdmin(Boolean isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public void setPassword(String password) {
