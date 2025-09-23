2bcf763a4685750f2b333604a4b7ff0f4c37ea26
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-09-23 14:21:13 -0300

    refactor: payment using VO Money

--- src/main/java/com/lumiere/domain/entity/Payment.java
+++ src/main/java/com/lumiere/domain/entity/Payment.java
@@ -1,41 +1,32 @@
 package com.lumiere.domain.entity;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Objects;
 import java.util.UUID;
 
+import com.lumiere.domain.vo.Money;
+
 public class Payment {
 
     private final UUID id;
-    private final BigDecimal amount;
-    private final String currency;
+    private final Money amount;
     private final PaymentMethod paymentMethod;
     private final LocalDateTime paymentDate;
 
-    private Payment(UUID id, BigDecimal amount, String currency, PaymentMethod paymentMethod,
-            LocalDateTime paymentDate) {
+    private Payment(UUID id, Money amount, PaymentMethod paymentMethod, LocalDateTime paymentDate) {
         this.id = id != null ? id : UUID.randomUUID();
         this.amount = Objects.requireNonNull(amount, "amount cannot be null");
-        if (amount.compareTo(BigDecimal.ZERO) <= 0) {
-            throw new IllegalArgumentException("amount must be positive");
-        }
-        this.currency = Objects.requireNonNull(currency, "currency cannot be null");
         this.paymentMethod = Objects.requireNonNull(paymentMethod, "paymentMethod cannot be null");
         this.paymentDate = Objects.requireNonNull(paymentDate, "paymentDate cannot be null");
     }
 
-    // Getters
+    // getters
     public UUID getId() {
         return id;
     }
 
-    public BigDecimal getAmount() {
+    public Money getAmount() {
         return amount;
-    }
-
-    public String getCurrency() {
-        return currency;
     }
 
     public PaymentMethod getPaymentMethod() {
@@ -46,13 +37,14 @@ public class Payment {
         return paymentDate;
     }
 
-    // Enum
+    // enums
+
     public enum PaymentMethod {
         PIX, CREDIT_CARD, DEBIT_CARD, BOLETO
     }
 
     // factory
-    public static Payment createPayment(BigDecimal amount, String currency, PaymentMethod paymentMethod) {
-        return new Payment(null, amount, currency, paymentMethod, LocalDateTime.now());
+    public static Payment createPayment(Money amount, PaymentMethod method) {
+        return new Payment(null, amount, method, LocalDateTime.now());
     }
 }
