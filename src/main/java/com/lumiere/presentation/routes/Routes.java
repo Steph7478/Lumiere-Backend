af6c1242defbfad5f6c033eddf2030d02fc00170
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-09-26 12:26:51 -0300

    feat: test/provisory routes

---
+++ src/main/java/com/lumiere/presentation/routes/Routes.java
@@ -0,0 +1,9 @@
+package com.lumiere.presentation.routes;
+
+public class Routes {
+
+    public static final String USERS = "/users";
+    public static final String PRODUCTS = "/products";
+    public static final String ADMIN = "/admin";
+
+}
