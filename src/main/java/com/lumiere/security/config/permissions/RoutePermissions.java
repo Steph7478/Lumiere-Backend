9fe39c4b18a1c5aecabf9dd6fbda72b74d05bf20
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-09-26 12:26:12 -0300

    feat: route permissions

---
+++ src/main/java/com/lumiere/security/config/permissions/RoutePermissions.java
@@ -0,0 +1,22 @@
+package com.lumiere.security.config.permissions;
+
+import com.lumiere.presentation.routes.Routes;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.HttpMethod;
+
+public class RoutePermissions {
+
+        public static final Map<String, Map<String, List<HttpMethod>>> ROUTE_PERMISSIONS = Map.of(
+                        Routes.USERS, Map.of(
+                                        "ADMIN",
+                                        List.of(HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE),
+                                        "USER", List.of(HttpMethod.GET, HttpMethod.POST)),
+                        Routes.PRODUCTS, Map.of(
+                                        "PUBLIC",
+                                        List.of(HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE)),
+                        Routes.ADMIN, Map.of(
+                                        "ADMIN",
+                                        List.of(HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE)));
+}
