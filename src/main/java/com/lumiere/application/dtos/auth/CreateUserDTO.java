b367ccad8102321f1e8623d5f2d32c0e76422fa5
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-09-26 18:14:27 -0300

    refactor: message to dto added

--- src/main/java/com/lumiere/application/dtos/auth/CreateUserDTO.java
+++ src/main/java/com/lumiere/application/dtos/auth/CreateUserDTO.java
@@ -6,9 +6,9 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record CreateUserDTO(
-                @NotBlank @Email String email,
+        @NotBlank @Email String email,
 
-                @NotBlank @Size(min = 3, max = 12) String name,
+        @NotBlank @Size(min = 3, max = 12, message = "Password must have between 3 to 12 letters") String name,
 
-                @NotBlank @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*]).{8,}$", message = "Password must be at least 8 characters long and contain letters, numbers, and a special character.") String password) {
+        @NotBlank @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*]).{8,}$", message = "Password must be at least 8 characters long and contain letters, numbers, and a special character.") String password) {
 }
