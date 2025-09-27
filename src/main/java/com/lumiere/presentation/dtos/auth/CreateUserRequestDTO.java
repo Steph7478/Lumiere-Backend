0a10c7071554e954fa30d73a6b0f5c7d49121960
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-09-27 18:05:05 -0300

    feat: create user dto

---
+++ src/main/java/com/lumiere/presentation/dtos/auth/CreateUserRequestDTO.java
@@ -0,0 +1,10 @@
+package com.lumiere.presentation.dtos.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record CreateUserRequestDTO(
+                @NotBlank @Email String email,
+                @NotBlank String name,
+                @NotBlank String password) {
+}
