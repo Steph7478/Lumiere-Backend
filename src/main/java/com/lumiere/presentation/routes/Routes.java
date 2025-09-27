02250e3812c53c24f19637f20fa2c9930814d0ef
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-09-27 16:46:17 -0300

    feat: register route

--- src/main/java/com/lumiere/presentation/routes/Routes.java
+++ src/main/java/com/lumiere/presentation/routes/Routes.java
@@ -2,7 +2,7 @@ package com.lumiere.presentation.routes;
 
 public class Routes {
 
-    public static final String USERS = "/users";
+    public static final String REGISTER = "/auth/register";
     public static final String PRODUCTS = "/products";
     public static final String ADMIN = "/admin";
 
