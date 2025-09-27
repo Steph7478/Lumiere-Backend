ecf5aab31d1e36048287fe894539a5e70cfa4690
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-09-27 16:41:51 -0300

    feat: endpoints script path

--- ops.sh
+++ ops.sh
@@ -4,5 +4,6 @@ SCRIPTS_DIR="$(dirname "$0")/src/main/java/com/lumiere/scripts"
 
 case $1 in
 	logs) "$SCRIPTS_DIR/FailureTestLog.sh";;
-	*) echo "Usage: ./ops.sh { logs }";;
+	endpoints)  "$SCRIPTS_DIR/Endpoints.sh";;
+	*) echo "Usage: ./ops.sh { logs } { endpoints }";;
 esac
