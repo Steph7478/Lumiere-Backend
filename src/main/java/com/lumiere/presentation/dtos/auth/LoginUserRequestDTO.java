a9dc4650517962f5863a28a637d8cb4764bdb7b2
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-11-01 15:56:10 -0300

    feat: login dto

---
+++ src/main/java/com/lumiere/presentation/dtos/auth/LoginUserRequestDTO.java
@@ -0,0 +1,10 @@
+package com.lumiere.presentation.dtos.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginUserRequestDTO(
+        @NotBlank @Email String email,
+        @NotBlank String password) {
+
+}
