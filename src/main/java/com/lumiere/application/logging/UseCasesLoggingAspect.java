1ef983904de49b0f49368ba092793866ee1ef883
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-11-02 12:23:12 -0300

    feat: new aspects for logging

---
+++ src/main/java/com/lumiere/application/logging/UseCasesLoggingAspect.java
@@ -0,0 +1,23 @@
+package com.lumiere.application.logging;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class UseCasesLoggingAspect {
+
+    @Before("@annotation(com.lumiere.shared.annotations.Loggable)")
+    public void logBefore(JoinPoint joinPoint) {
+        System.out.println("[USECASE LOG] Entering: " + joinPoint.getSignature().toShortString());
+    }
+
+    @AfterReturning(pointcut = "execution(* com.lumiere.application.usecase..*(..))", returning = "result")
+    public void logAfter(JoinPoint joinPoint, Object result) {
+        System.out
+                .println("[USECASE LOG] Exiting: " + joinPoint.getSignature().toShortString() + " | Result: " + result);
+    }
+}
---
+++ src/main/java/com/lumiere/presentation/logging/ControllerLoggingAspect.java
@@ -0,0 +1,27 @@
+package com.lumiere.presentation.logging;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ControllerLoggingAspect {
+
+    @Around("@annotation(com.lumiere.shared.annotations.Loggable)")
+    public Object logControllerExecution(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        try {
+            Object result = joinPoint.proceed();
+            long elapsed = System.currentTimeMillis() - start;
+            System.out.println("[CONTROLLER LOG] " + joinPoint.getSignature() +
+                    " executed in " + elapsed + "ms | Result: " + result);
+            return result;
+        } catch (Throwable ex) {
+            System.out.println("[CONTROLLER ERROR] " + joinPoint.getSignature() +
+                    " threw " + ex.getClass().getSimpleName() + ": " + ex.getMessage());
+            throw ex;
+        }
+    }
+}
