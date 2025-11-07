e3c36317b6c009160bf7f0c3943ae72cc08afe12
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-11-07 10:34:35 -0300

    feat: finduserinfo jpa repo

--- src/main/java/com/lumiere/domain/repositories/UserRepository.java
+++ src/main/java/com/lumiere/domain/repositories/UserRepository.java
@@ -5,10 +5,14 @@ import java.util.UUID;
 
 import com.lumiere.domain.entities.User;
 import com.lumiere.domain.readmodels.AuthInfoView;
+import com.lumiere.domain.readmodels.UserInfoView;
 import com.lumiere.domain.repositories.base.BaseRepository;
 
 public interface UserRepository extends BaseRepository<User> {
     Optional<User> findByAuthEmail(String email);
 
+    Optional<UserInfoView> findUserInfoById(UUID id);
+
     Optional<AuthInfoView> findAuthInfoByAuthId(UUID id);
+
 }
