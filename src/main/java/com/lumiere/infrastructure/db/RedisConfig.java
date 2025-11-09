520b188190545a8c28df61df9b70063467d4a07c
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-11-09 17:33:54 -0300

    refactor: package path redis

--- src/main/java/com/lumiere/infrastructure/db/RedisConfig.java
+++ src/main/java/com/lumiere/infrastructure/db/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.lumiere.infrastructure.security.config;
+package com.lumiere.infrastructure.db;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
