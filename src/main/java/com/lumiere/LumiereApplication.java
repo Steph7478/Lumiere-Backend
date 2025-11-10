ad63f1c84b65bb30db5df20ad65cb8de5c76c29f
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-11-10 17:00:18 -0300

    feat: enable caching

--- src/main/java/com/lumiere/LumiereApplication.java
+++ src/main/java/com/lumiere/LumiereApplication.java
@@ -2,10 +2,12 @@ package com.lumiere;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 import com.lumiere.infrastructure.security.config.DotenvLoader;
 
 @SpringBootApplication
+@EnableCaching
 public class LumiereApplication {
 
 	public static void main(String[] args) {
