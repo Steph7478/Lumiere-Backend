545bbef4919a46c6753e865e6c7c8b9a70147fab
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-11-10 17:01:10 -0300

    feat: implements serializable

--- src/main/java/com/lumiere/infrastructure/persistence/jpa/entities/AuthJpaEntity.java
+++ src/main/java/com/lumiere/infrastructure/persistence/jpa/entities/AuthJpaEntity.java
@@ -3,6 +3,7 @@ package com.lumiere.infrastructure.persistence.jpa.entities;
 import jakarta.persistence.*;
 import lombok.Getter;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -10,7 +11,7 @@ import com.lumiere.infrastructure.persistence.jpa.entities.base.BaseJpaEntity;
 
 @Getter
 @Entity
-public class AuthJpaEntity extends BaseJpaEntity {
+public class AuthJpaEntity extends BaseJpaEntity implements Serializable {
 
     @OneToOne(mappedBy = "auth", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JsonIgnore
