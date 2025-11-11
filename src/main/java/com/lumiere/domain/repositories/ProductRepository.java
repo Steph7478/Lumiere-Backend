c2f3e7b0e93f6998ca9e7e213ba3cf15778a1760
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-11-11 11:10:50 -0300

    feat: productcategory repo

---
+++ src/main/java/com/lumiere/domain/repositories/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.lumiere.domain.repositories;
+
+import com.lumiere.domain.entities.Product;
+import com.lumiere.domain.repositories.base.BaseRepository;
+
+public interface ProductRepository extends BaseRepository<Product> {
+
+}
