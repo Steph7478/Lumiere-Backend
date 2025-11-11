7713a8e87efe4540231004800302bc235eb8d965
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-11-11 11:11:03 -0300

    feat: productcategory service

---
+++ src/main/java/com/lumiere/domain/services/ProductCategoryService.java
@@ -0,0 +1,13 @@
+package com.lumiere.domain.services;
+
+import java.util.UUID;
+
+import com.lumiere.domain.entities.ProductCategory;
+import com.lumiere.domain.enums.CategoriesEnum.Category;
+import com.lumiere.domain.enums.CategoriesEnum.SubCategory;
+
+public class ProductCategoryService {
+    public ProductCategory createProductCategory(UUID productId, Category category, SubCategory subcategory) {
+        return ProductCategory.createProductCategory(productId, category, subcategory);
+    }
+}
