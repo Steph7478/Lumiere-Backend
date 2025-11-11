164a6779d010c637539b30939ceb191ef107fc0e
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-11-11 20:45:13 -0300

    feat: redis config mapper

--- src/main/java/com/lumiere/infrastructure/persistence/nosql/db/RedisConfig.java
+++ src/main/java/com/lumiere/infrastructure/persistence/nosql/db/RedisConfig.java
@@ -7,19 +7,26 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.lumiere.domain.entities.ProductCategory;
 
 import java.util.UUID;
 
 @Configuration
 public class RedisConfig {
+
     @Bean
     public RedisTemplate<String, ProductCategory> productCategoryRedisTemplate(RedisConnectionFactory cf) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+
+        GenericJackson2JsonRedisSerializer jsonSerializer = new GenericJackson2JsonRedisSerializer(objectMapper);
 
         RedisTemplate<String, ProductCategory> template = new RedisTemplate<>();
         template.setConnectionFactory(cf);
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setValueSerializer(jsonSerializer);
 
         template.afterPropertiesSet();
         return template;
@@ -27,25 +34,21 @@ public class RedisConfig {
 
     @Bean
     public RedisTemplate<String, String> customStringRedisTemplate(RedisConnectionFactory cf) {
-
         RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(cf);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new StringRedisSerializer());
         template.afterPropertiesSet();
-
         return template;
     }
 
     @Bean
     public RedisTemplate<String, UUID> uuidRedisTemplate(RedisConnectionFactory cf) {
-
         RedisTemplate<String, UUID> template = new RedisTemplate<>();
         template.setConnectionFactory(cf);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericToStringSerializer<>(UUID.class));
         template.afterPropertiesSet();
-
         return template;
     }
 }
