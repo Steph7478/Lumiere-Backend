a3594a93cfdf125d2401b1b369510ad486d64f5a
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-11-13 10:05:22 -0300

    feat: modify product interface

---
+++ src/main/java/com/lumiere/application/interfaces/admin/IModifyProduct.java
@@ -0,0 +1,9 @@
+package com.lumiere.application.interfaces.admin;
+
+import com.lumiere.application.dtos.admin.command.modify.ModifyProductInput;
+import com.lumiere.application.dtos.admin.command.modify.ModifyProductOutput;
+import com.lumiere.application.interfaces.base.BaseUseCase;
+
+public interface IModifyProduct extends BaseUseCase<ModifyProductInput, ModifyProductOutput> {
+
+}
