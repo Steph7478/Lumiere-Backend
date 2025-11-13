dca62cb9a5574b5fc3da544c463d2130fc15388e
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-11-13 10:05:49 -0300

    feat: modify controller

--- src/main/java/com/lumiere/presentation/controllers/AdminController.java
+++ src/main/java/com/lumiere/presentation/controllers/AdminController.java
@@ -1,15 +1,23 @@
 package com.lumiere.presentation.controllers;
 
+import java.util.UUID;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lumiere.application.dtos.admin.command.add.AddProductInput;
 import com.lumiere.application.dtos.admin.command.add.AddProductOutput;
+import com.lumiere.application.dtos.admin.command.modify.ModifyProductInput;
+import com.lumiere.application.dtos.admin.command.modify.ModifyProductOutput;
 import com.lumiere.application.usecases.admin.AddProductUseCase;
+import com.lumiere.application.usecases.admin.ModifyProductUseCase;
 import com.lumiere.presentation.controllers.base.BaseController;
 import com.lumiere.presentation.routes.Routes;
 
@@ -19,9 +27,11 @@ import jakarta.validation.Valid;
 @RestController
 public class AdminController extends BaseController {
     private final AddProductUseCase addProductUseCase;
+    private final ModifyProductUseCase modifyProductUseCase;
 
-    public AdminController(AddProductUseCase addProductUseCase) {
+    public AdminController(AddProductUseCase addProductUseCase, ModifyProductUseCase modifyProductUseCase) {
         this.addProductUseCase = addProductUseCase;
+        this.modifyProductUseCase = modifyProductUseCase;
     }
 
     @PreAuthorize("hasRole('ADMIN') and hasAuthority('PRODUCT_ADD')")
@@ -31,5 +41,35 @@ public class AdminController extends BaseController {
 
         AddProductOutput appDTO = addProductUseCase.execute(req);
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(appDTO);
+    }
+
+    @PreAuthorize("hasRole('ADMIN') and hasAuthority('PRODUCT_ADD')")
+    @PutMapping(Routes.PRIVATE.ADMIN.UPDATE_PRODUCT)
+    public ResponseEntity<ModifyProductOutput> putProduct(@PathVariable UUID id,
+            @Valid @RequestBody ModifyProductInput req,
+            HttpServletResponse res) {
+        if (!req.isCompleteUpdate())
+            return ResponseEntity.badRequest().build();
+
+        ModifyProductInput appDTO = new ModifyProductInput(id, req.name(), req.description());
+
+        ModifyProductOutput responseDTO = modifyProductUseCase.execute(appDTO);
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(responseDTO);
+    }
+
+    @PreAuthorize("hasRole('ADMIN') and hasAuthority('PRODUCT_ADD')")
+    @PatchMapping(Routes.PRIVATE.ADMIN.UPDATE_PRODUCT)
+    public ResponseEntity<ModifyProductOutput> patchProduct(@PathVariable UUID id,
+            @Valid @RequestBody ModifyProductInput req,
+            HttpServletResponse res) {
+        if (!req.hasUpdates())
+            return ResponseEntity.badRequest().build();
+
+        ModifyProductInput appDTO = new ModifyProductInput(id, req.name(), req.description());
+
+        ModifyProductOutput responseDTO = modifyProductUseCase.execute(appDTO);
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(responseDTO);
     }
 }
