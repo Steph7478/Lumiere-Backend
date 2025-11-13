3162948ad8687071c6b9f4fc1271484f7da8ed8d
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-11-13 16:08:45 -0300

    feat increasestock usecase

---
+++ src/main/java/com/lumiere/application/usecases/admin/IncreaseStockUseCase.java
@@ -0,0 +1,35 @@
+package com.lumiere.application.usecases.admin;
+
+import org.springframework.stereotype.Service;
+
+import com.lumiere.application.dtos.admin.command.stock.increase.IncreaseStockInput;
+import com.lumiere.application.dtos.admin.command.stock.increase.IncreaseStockOutput;
+import com.lumiere.application.exceptions.product.ProductNotFoundException;
+import com.lumiere.application.interfaces.admin.IIncreaseStockUseCase;
+import com.lumiere.domain.entities.Product;
+import com.lumiere.domain.repositories.ProductRepository;
+import com.lumiere.domain.services.ProductService;
+import com.lumiere.shared.annotations.validators.requireAdmin.RequireAdmin;
+
+@Service
+public class IncreaseStockUseCase implements IIncreaseStockUseCase {
+    private final ProductRepository productRepository;
+
+    public IncreaseStockUseCase(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    @RequireAdmin
+    public IncreaseStockOutput execute(IncreaseStockInput input) {
+        Product product = productRepository.findById(input.id())
+                .orElseThrow(() -> new ProductNotFoundException(input.id()));
+
+        ProductService.increaseStock(product, input.quantity());
+
+        productRepository.save(product);
+
+        return new IncreaseStockOutput(product.getId(), product.getStock());
+    }
+
+}
