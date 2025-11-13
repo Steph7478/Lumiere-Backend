5967a2b5c564787a922f0707473dbe3808a73b78
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-11-13 18:53:46 -0300

    delete: unused import

--- src/main/java/com/lumiere/infrastructure/security/config/JacksonConfig.java
+++ src/main/java/com/lumiere/infrastructure/security/config/JacksonConfig.java
@@ -3,7 +3,6 @@ package com.lumiere.infrastructure.security.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
