9daada1b7ca8c6d0d373a5a71f79658dcd435408
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-11-13 09:21:15 -0300

    refactor: moving service

--- src/main/java/com/lumiere/domain/services/ProductService.java
+++ src/main/java/com/lumiere/domain/services/ProductService.java
@@ -1,5 +1,7 @@
 package com.lumiere.domain.services;
 
+import java.util.Optional;
+
 import com.lumiere.domain.entities.Product;
 import com.lumiere.domain.vo.Money;
 import com.lumiere.domain.vo.Stock;
@@ -17,4 +19,19 @@ public class ProductService {
         return Product.createProduct(name, description, price, stock);
     }
 
+    public static void update(Product product, Optional<String> name, Optional<String> description) {
+        product.updateProduct(name, description);
+    }
+
+    public static Product increaseStock(Product product, int quantity) {
+        return product.increaseStock(quantity);
+    }
+
+    public static Product decreaseStock(Product product, int quantity) {
+        return product.decreaseStock(quantity);
+    }
+
+    public static Product updatePrice(Product product, Money newPrice) {
+        return product.updatePrice(newPrice);
+    }
 }
