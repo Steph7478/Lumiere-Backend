d9858d1ae31f5dfc434cce4fc7cd9588aff344f9
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-11-13 16:06:56 -0300

    refactor: modify interface renamed

--- src/main/java/com/lumiere/application/interfaces/admin/IModifyProductUseCase.java
+++ src/main/java/com/lumiere/application/interfaces/admin/IModifyProductUseCase.java
@@ -4,6 +4,6 @@ import com.lumiere.application.dtos.admin.command.modify.ModifyProductInput;
 import com.lumiere.application.dtos.admin.command.modify.ModifyProductOutput;
 import com.lumiere.application.interfaces.base.BaseUseCase;
 
-public interface IModifyProduct extends BaseUseCase<ModifyProductInput, ModifyProductOutput> {
+public interface IModifyProductUseCase extends BaseUseCase<ModifyProductInput, ModifyProductOutput> {
 
 }
