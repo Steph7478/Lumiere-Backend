7fb531f0232b45ad2a83f121471c6f116e984047
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-11-17 10:45:06 -0300

    refactor: better type to mapper enum

--- src/main/java/com/lumiere/infrastructure/mappers/ProductMapper.java
+++ src/main/java/com/lumiere/infrastructure/mappers/ProductMapper.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 
 import com.lumiere.domain.entities.Product;
 import com.lumiere.domain.entities.ProductCategory;
+import com.lumiere.domain.enums.CategoriesEnum;
 import com.lumiere.domain.enums.CurrencyEnum.CurrencyType;
 import com.lumiere.domain.readmodels.ProductDetailReadModel;
 import com.lumiere.domain.vo.Money;
@@ -16,52 +17,53 @@ import java.util.Collections;
 @Component
 public final class ProductMapper implements BaseMapper<Product, ProductJpaEntity> {
 
-    @Override
-    public ProductJpaEntity toJpa(Product domain) {
-        return new ProductJpaEntity(
-                domain.getId(),
-                domain.getName(),
-                domain.getDescription(),
-                domain.getPrice().getAmount(),
-                domain.getPrice().getCurrency().name(),
-                domain.getStock().getQuantity());
-    }
+        @Override
+        public ProductJpaEntity toJpa(Product domain) {
+                return new ProductJpaEntity(
+                                domain.getId(),
+                                domain.getName(),
+                                domain.getDescription(),
+                                domain.getPrice().getAmount(),
+                                domain.getPrice().getCurrency().name(),
+                                domain.getStock().getQuantity());
+        }
 
-    @Override
-    public Product toDomain(ProductJpaEntity jpaEntity) {
-        Money price = new Money(
-                jpaEntity.getPriceAmount(),
-                CurrencyType.valueOf(jpaEntity.getPriceCurrency()));
-        Stock stock = new Stock(jpaEntity.getStockQuantity());
+        @Override
+        public Product toDomain(ProductJpaEntity jpaEntity) {
+                Money price = new Money(
+                                jpaEntity.getPriceAmount(),
+                                CurrencyType.valueOf(jpaEntity.getPriceCurrency()));
+                Stock stock = new Stock(jpaEntity.getStockQuantity());
 
-        return Product.from(
-                jpaEntity.getId(),
-                jpaEntity.getName(),
-                jpaEntity.getDescription(),
-                price,
-                stock);
-    }
+                return Product.from(
+                                jpaEntity.getId(),
+                                jpaEntity.getName(),
+                                jpaEntity.getDescription(),
+                                price,
+                                stock);
+        }
 
-    public ProductDetailReadModel toReadModel(
-            ProductJpaEntity jpaEntity,
-            ProductCategory nosqlCategory) {
-        Money price = new Money(
-                jpaEntity.getPriceAmount(),
-                CurrencyType.valueOf(jpaEntity.getPriceCurrency()));
+        public ProductDetailReadModel toReadModel(
+                        ProductJpaEntity jpaEntity,
+                        ProductCategory nosqlCategory) {
+                Money price = new Money(
+                                jpaEntity.getPriceAmount(),
+                                CurrencyType.valueOf(jpaEntity.getPriceCurrency()));
 
-        final var category = nosqlCategory != null ? nosqlCategory.getCategory() : null;
-        final var subCategory = nosqlCategory != null ? nosqlCategory.getSubcategory() : null;
+                final CategoriesEnum.Category category = nosqlCategory != null ? nosqlCategory.getCategory() : null;
+                final CategoriesEnum.SubCategory subCategory = nosqlCategory != null ? nosqlCategory.getSubcategory()
+                                : null;
 
-        return new ProductDetailReadModel(
-                jpaEntity.getId() != null ? jpaEntity.getId().toString() : null,
-                jpaEntity.getName(),
-                jpaEntity.getDescription(),
-                price,
-                Collections.emptyList(),
-                jpaEntity.getStockQuantity(),
-                jpaEntity.getCreatedAt(),
-                jpaEntity.getUpdatedAt(),
-                category,
-                subCategory);
-    }
+                return new ProductDetailReadModel(
+                                jpaEntity.getId() != null ? jpaEntity.getId().toString() : null,
+                                jpaEntity.getName(),
+                                jpaEntity.getDescription(),
+                                price,
+                                Collections.emptyList(),
+                                jpaEntity.getStockQuantity(),
+                                jpaEntity.getCreatedAt(),
+                                jpaEntity.getUpdatedAt(),
+                                category,
+                                subCategory);
+        }
 }
