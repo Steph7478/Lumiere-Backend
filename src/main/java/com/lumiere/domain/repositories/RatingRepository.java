31c060d76ae863d6bbc63e87e28fa41c9025d395
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-11-19 14:34:37 -0300

    feat: rating interface

---
+++ src/main/java/com/lumiere/domain/repositories/RatingRepository.java
@@ -0,0 +1,8 @@
+package com.lumiere.domain.repositories;
+
+import com.lumiere.domain.entities.Rating;
+import com.lumiere.domain.repositories.base.BaseRepository;
+
+public interface RatingRepository extends BaseRepository<Rating> {
+
+}
