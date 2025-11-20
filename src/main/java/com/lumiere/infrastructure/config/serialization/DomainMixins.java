0c266c9d9afdf5f8c0257f1cb50ac3231741c05f
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-11-19 21:32:11 -0300

    refactor: stock

--- src/main/java/com/lumiere/infrastructure/config/serialization/DomainMixins.java
+++ src/main/java/com/lumiere/infrastructure/config/serialization/DomainMixins.java
@@ -1,9 +1,9 @@
 package com.lumiere.infrastructure.config.serialization;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue; // <--- Importação necessária
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.lumiere.domain.enums.CurrencyEnum;
+
 import java.math.BigDecimal;
 
 public abstract class DomainMixins {
@@ -18,10 +18,8 @@ public abstract class DomainMixins {
 
     public static abstract class StockMixin {
         @JsonCreator
-        public StockMixin(int quantity) {
+        public StockMixin(
+                @JsonProperty("quantity") int quantity) {
         }
-
-        @JsonValue
-        public abstract int getQuantity();
     }
 }
