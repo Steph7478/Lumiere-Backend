8be7d4498edb938a41c00b78f8d790a98d6cb272
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-11-20 09:34:03 -0300

    fix: auth mapper

--- src/main/java/com/lumiere/infrastructure/mappers/AuthMapper.java
+++ src/main/java/com/lumiere/infrastructure/mappers/AuthMapper.java
@@ -3,14 +3,11 @@ package com.lumiere.infrastructure.mappers;
 import com.lumiere.domain.entities.Auth;
 import com.lumiere.infrastructure.mappers.base.BaseMapper;
 import com.lumiere.infrastructure.persistence.jpa.entities.AuthJpaEntity;
+
 import org.mapstruct.*;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, uses = { UserMapper.class })
 public interface AuthMapper extends BaseMapper<Auth, AuthJpaEntity> {
-
-    @Mapping(target = "user", ignore = true)
-    @Mapping(target = "withPasswordHash", ignore = true)
-    Auth toDomain(AuthJpaEntity jpa);
 
     @Mapping(target = "password", source = "passwordHash")
     @Mapping(target = "isAdmin", source = "admin")
@@ -25,4 +22,11 @@ public interface AuthMapper extends BaseMapper<Auth, AuthJpaEntity> {
                 jpa.getIsAdmin() != null && jpa.getIsAdmin(),
                 jpa.getId());
     }
+
+    @Mapping(target = "admin", ignore = true)
+    @Mapping(target = "name", ignore = true)
+    @Mapping(target = "email", ignore = true)
+    @Mapping(target = "passwordHash", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    Auth toDomain(AuthJpaEntity jpa);
 }
