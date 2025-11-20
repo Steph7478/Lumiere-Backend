41a65c07beec41255ec3c5d8d95c9abd52d8788d
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-11-20 13:28:33 -0300

    refactor: reducing code caritem

--- src/main/java/com/lumiere/infrastructure/mappers/CartItemMapper.java
+++ src/main/java/com/lumiere/infrastructure/mappers/CartItemMapper.java
@@ -6,7 +6,6 @@ import com.lumiere.infrastructure.persistence.jpa.entities.CartItemJpaEntity;
 import com.lumiere.infrastructure.persistence.jpa.entities.CartJpaEntity;
 import com.lumiere.infrastructure.persistence.jpa.entities.ProductJpaEntity;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 import java.util.UUID;
@@ -14,28 +13,17 @@ import java.util.UUID;
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface CartItemMapper extends BaseMapper<CartItem, CartItemJpaEntity> {
 
-        @Mapping(target = "productId", source = "product.id")
-        @Mapping(target = "quantity", source = "quantity")
         CartItem toDomain(CartItemJpaEntity jpaEntity);
 
-        @Mapping(target = "id", ignore = true)
-        @Mapping(target = "cart", source = "cartJpaEntity")
-        @Mapping(target = "product", source = "productJpaEntity")
-        @Mapping(target = "quantity", source = "domain.quantity")
-        CartItemJpaEntity toJpa(
+        default CartItemJpaEntity toJpa(
                         CartItem domain,
                         CartJpaEntity cartJpaEntity,
-                        ProductJpaEntity productJpaEntity);
-
-        default CartItemJpaEntity createJpaEntity(
-                        CartJpaEntity cart,
-                        ProductJpaEntity product,
-                        Integer quantity) {
+                        ProductJpaEntity productJpaEntity) {
 
                 return new CartItemJpaEntity(
                                 UUID.randomUUID(),
-                                cart,
-                                product,
-                                quantity);
+                                cartJpaEntity,
+                                productJpaEntity,
+                                domain.getQuantity());
         }
 }
