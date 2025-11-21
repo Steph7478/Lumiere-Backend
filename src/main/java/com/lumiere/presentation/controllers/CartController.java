933f39155f196af1e3dd7acff4b6a6fbdda7d097
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-11-21 11:02:40 -0300

    feat: cart controller

---
+++ src/main/java/com/lumiere/presentation/controllers/CartController.java
@@ -0,0 +1,35 @@
+package com.lumiere.presentation.controllers;
+
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lumiere.application.dtos.cart.command.add.AddCartInput;
+import com.lumiere.application.dtos.cart.command.add.AddCartOuput;
+import com.lumiere.application.dtos.cart.command.add.AddCartRequestData;
+import com.lumiere.application.interfaces.cart.IAddCartUseCase;
+import com.lumiere.presentation.controllers.base.BaseController;
+import com.lumiere.presentation.routes.Routes;
+
+@RestController
+public class CartController extends BaseController {
+
+    private final IAddCartUseCase addCartUseCase;
+
+    protected CartController(IAddCartUseCase addCartUseCase) {
+        this.addCartUseCase = addCartUseCase;
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+    @PostMapping(Routes.PRIVATE.CART.ADD_CART)
+    public ResponseEntity<AddCartOuput> addToCart(@AuthenticationPrincipal UUID id, AddCartRequestData requestData) {
+        AddCartInput request = new AddCartInput(id, requestData);
+        AddCartOuput response = addCartUseCase.execute(request);
+
+        return ResponseEntity.ok(response);
+    }
+}
