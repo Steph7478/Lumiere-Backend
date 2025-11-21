702e5eb64f1e4e18db2d5810b48e2134dc210286
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-11-21 13:09:12 -0300

    fix: this.user

--- src/main/java/com/lumiere/infrastructure/persistence/jpa/entities/CartJpaEntity.java
+++ src/main/java/com/lumiere/infrastructure/persistence/jpa/entities/CartJpaEntity.java
@@ -31,9 +31,9 @@ public class CartJpaEntity extends BaseJpaEntity implements Serializable {
     @OneToMany(mappedBy = "cart")
     private List<CartItemJpaEntity> items;
 
-    public CartJpaEntity(UUID id, UserJpaEntity userId, String coupon, List<CartItemJpaEntity> items) {
+    public CartJpaEntity(UUID id, UserJpaEntity user, String coupon, List<CartItemJpaEntity> items) {
         super(id);
-        this.user = userId;
+        this.user = user;
         this.coupon = coupon;
         this.items = items;
     }
