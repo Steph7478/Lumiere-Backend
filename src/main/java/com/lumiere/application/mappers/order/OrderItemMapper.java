5da08ec7f89127fba25bae8ffc474d6d97e621c0
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-11-24 10:24:48 -0300

    feat: orderMapper

---
+++ src/main/java/com/lumiere/application/mappers/order/OrderItemMapper.java
@@ -0,0 +1,19 @@
+package com.lumiere.application.mappers.order;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import com.lumiere.application.dtos.order.command.create.CreateOrderRequestData;
+import com.lumiere.domain.entities.Product;
+import com.lumiere.domain.vo.OrderItem;
+
+@Mapper(componentModel = "spring")
+public interface OrderItemMapper {
+
+    @Mapping(source = "product.id", target = "productId")
+    @Mapping(source = "product.name", target = "name")
+    @Mapping(source = "requestData.quantity", target = "quantity")
+    @Mapping(source = "product.price.amount", target = "unitPrice")
+    @Mapping(source = "product.price.currency", target = "currency")
+    OrderItem toOrderItem(Product product, CreateOrderRequestData requestData);
+}
