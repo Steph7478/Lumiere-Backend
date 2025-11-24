f21a13776d7b7a4f11b2315e65ebb326ad46ac92
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-11-24 12:33:14 -0300

    delete: unused imports

--- src/main/java/com/lumiere/infrastructure/persistence/jpa/repositories/cart/CartJpaRepositoryAdapter.java
+++ src/main/java/com/lumiere/infrastructure/persistence/jpa/repositories/cart/CartJpaRepositoryAdapter.java
@@ -9,11 +9,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.lumiere.domain.entities.Cart;
 import com.lumiere.domain.repositories.CartRepository;
 import com.lumiere.infrastructure.mappers.CartMapper;
-import com.lumiere.infrastructure.mappers.CartItemMapper;
 import com.lumiere.infrastructure.persistence.jpa.entities.CartJpaEntity;
 import com.lumiere.infrastructure.persistence.jpa.repositories.base.BaseRepositoryAdapter;
-import com.lumiere.infrastructure.persistence.jpa.repositories.product.ProductJpaRepository;
-import com.lumiere.infrastructure.persistence.jpa.repositories.user.UserJpaRepository;
 
 import jakarta.persistence.EntityManager;
 
@@ -26,10 +23,7 @@ public class CartJpaRepositoryAdapter extends BaseRepositoryAdapter<Cart, CartJp
     protected CartJpaRepositoryAdapter(
             CartJpaRepository jpaRepository,
             CartMapper mapper,
-            EntityManager entityManager,
-            ProductJpaRepository productJpaRepository,
-            UserJpaRepository userJpaRepository,
-            CartItemMapper cartItemMapper) {
+            EntityManager entityManager) {
         super(jpaRepository, mapper, entityManager, CartJpaEntity.class);
 
         this.cartJpaRepository = jpaRepository;
