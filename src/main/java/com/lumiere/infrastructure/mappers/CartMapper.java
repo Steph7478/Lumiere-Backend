64fadd854a5a261eade40ddfb8a890e585433a9d
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-11-27 14:34:17 -0300

    refactor: reducing code cartmapper

--- src/main/java/com/lumiere/infrastructure/mappers/CartMapper.java
+++ src/main/java/com/lumiere/infrastructure/mappers/CartMapper.java
@@ -3,33 +3,25 @@ package com.lumiere.infrastructure.mappers;
 import com.lumiere.domain.entities.Cart;
 import com.lumiere.domain.readmodels.CartItemReadModel;
 import com.lumiere.domain.readmodels.CartReadModel;
-import com.lumiere.infrastructure.persistence.jpa.entities.CartJpaEntity;
+import com.lumiere.domain.vo.CartItem;
 import com.lumiere.infrastructure.mappers.base.BaseMapper;
 import com.lumiere.infrastructure.persistence.jpa.entities.CartItemJpaEntity;
+import com.lumiere.infrastructure.persistence.jpa.entities.CartJpaEntity;
 import com.lumiere.infrastructure.persistence.jpa.entities.ProductJpaEntity;
 import com.lumiere.infrastructure.persistence.jpa.repositories.product.ProductJpaRepository;
-import com.lumiere.infrastructure.persistence.jpa.repositories.user.UserJpaRepository;
 import org.mapstruct.AfterMapping;
-import org.mapstruct.BeforeMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.ReportingPolicy;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring", uses = { CartItemMapper.class,
                 UserMapper.class }, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public abstract class CartMapper implements BaseMapper<Cart, CartJpaEntity> {
-
-        @Autowired
-        protected UserJpaRepository userJpaRepository;
 
         @Autowired
         protected ProductJpaRepository productJpaRepository;
@@ -41,40 +33,43 @@ public abstract class CartMapper implements BaseMapper<Cart, CartJpaEntity> {
         @Mapping(target = "user", source = "domain.user")
         public abstract CartJpaEntity mapToJpa(Cart domain);
 
-        @BeforeMapping
-        protected Map<UUID, ProductJpaEntity> loadProductsForCart(Cart domain) {
-                return productJpaRepository.findAllByIdIn(Objects.requireNonNull(
-                                domain.getItems().stream()
-                                                .map(i -> i.getProductId())
-                                                .collect(Collectors.toSet())))
-                                .stream()
+        public CartReadModel toReadModel(Cart domain) {
+                var productCache = loadProducts(domain.getItems());
+
+                var items = domain.getItems().stream()
+                                .map(i -> new CartItemReadModel(
+                                                productCache.get(i.getProductId()),
+                                                i.getQuantity()))
+                                .toList();
+
+                return new CartReadModel(
+                                domain.getId(),
+                                items,
+                                domain.getCreatedAt(),
+                                domain.getUpdatedAt());
+        }
+
+        protected Map<UUID, ProductJpaEntity> loadProducts(List<CartItem> items) {
+                var ids = items.stream()
+                                .map(CartItem::getProductId)
+                                .collect(Collectors.toSet());
+
+                return productJpaRepository.findAllByIdIn(Objects.requireNonNull(ids)).stream()
                                 .collect(Collectors.toMap(ProductJpaEntity::getId, p -> p));
         }
 
         @AfterMapping
-        protected void mapItemsAndLink(Cart domain, @MappingTarget CartJpaEntity jpaEntity) {
-                Map<UUID, ProductJpaEntity> productCache = loadProductsForCart(domain);
+        protected void mapItems(Cart domain, @MappingTarget CartJpaEntity jpa) {
+                var productCache = loadProducts(domain.getItems());
 
-                jpaEntity.setItems(domain.getItems().stream()
-                                .map(i -> {
-                                        CartItemJpaEntity itemJpa = cartItemMapper.toJpa(i, productCache);
-                                        itemJpa.setCartReference(jpaEntity);
+                var items = domain.getItems().stream()
+                                .map(item -> {
+                                        var itemJpa = cartItemMapper.toJpa(item, productCache);
+                                        itemJpa.setCartReference(jpa);
                                         return itemJpa;
                                 })
-                                .collect(Collectors.toList()));
+                                .toList();
+
+                jpa.setItems(items);
         }
-
-        public CartReadModel toReadModel(Cart domain) {
-                Map<UUID, ProductJpaEntity> productCache = loadProductsForCart(domain);
-
-                List<CartItemReadModel> items = Optional.ofNullable(domain.getItems())
-                                .orElse(List.of())
-                                .stream()
-                                .map(item -> new CartItemReadModel(productCache.get(item.getProductId()),
-                                                item.getQuantity()))
-                                .collect(Collectors.toList());
-
-                return new CartReadModel(domain.getId(), items, domain.getCreatedAt(), domain.getUpdatedAt());
-        }
-
 }
