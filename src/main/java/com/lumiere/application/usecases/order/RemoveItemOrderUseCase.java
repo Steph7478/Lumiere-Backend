381f02f1b72071789a46c44df1428e7df8c58baa
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-11-28 17:17:42 -0300

    feat: remove item usecase

---
+++ src/main/java/com/lumiere/application/usecases/order/RemoveItemOrderUseCase.java
@@ -0,0 +1,54 @@
+package com.lumiere.application.usecases.order;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.lumiere.application.dtos.order.command.remove.RemoveItemOrderInput;
+import com.lumiere.application.dtos.order.command.remove.RemoveItemOutput;
+import com.lumiere.application.exceptions.auth.UserNotFoundException;
+import com.lumiere.application.exceptions.order.OrderNotFoundException;
+import com.lumiere.application.interfaces.order.IRemoveItemOrderUseCase;
+import com.lumiere.domain.entities.Order;
+import com.lumiere.domain.entities.User;
+import com.lumiere.domain.enums.StatusEnum.Status;
+import com.lumiere.domain.repositories.OrderRepository;
+import com.lumiere.domain.repositories.UserRepository;
+import com.lumiere.domain.services.OrderService;
+import com.lumiere.domain.vo.OrderItem;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class RemoveItemOrderUseCase implements IRemoveItemOrderUseCase {
+    private final UserRepository userRepo;
+    private final OrderRepository orderRepo;
+
+    protected RemoveItemOrderUseCase(UserRepository userRepo, OrderRepository orderRepo) {
+        this.orderRepo = orderRepo;
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    @Transactional
+    public RemoveItemOutput execute(RemoveItemOrderInput input) {
+        User user = userRepo.findUserByAuthId(input.userId()).orElseThrow(UserNotFoundException::new);
+
+        Order order = orderRepo.findByUserIdAndStatus(user.getId(), Status.IN_PROGRESS)
+                .orElseThrow(OrderNotFoundException::new);
+
+        List<UUID> idsToRemove = input.items().productsId();
+
+        List<OrderItem> itemsToRemove = order.getItems().stream()
+                .filter(item -> idsToRemove.contains(item.getProductId()))
+                .toList();
+
+        Order updatedOrder = OrderService.removeProducts(order, itemsToRemove);
+
+        orderRepo.update(updatedOrder);
+
+        return new RemoveItemOutput();
+    }
+
+}
