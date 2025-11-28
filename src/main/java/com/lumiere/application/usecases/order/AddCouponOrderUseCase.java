f9dfb1883c397fbf91dd73b9f62fb7e30a8e008e
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-11-28 20:04:44 -0300

    feat: coupon usecase

---
+++ src/main/java/com/lumiere/application/usecases/order/AddCouponOrderUseCase.java
@@ -0,0 +1,37 @@
+package com.lumiere.application.usecases.order;
+
+import com.lumiere.application.dtos.order.command.coupon.AddCouponInput;
+import com.lumiere.application.dtos.order.command.coupon.AddCouponOutput;
+import com.lumiere.application.exceptions.auth.UserNotFoundException;
+import com.lumiere.application.exceptions.order.OrderNotFoundException;
+import com.lumiere.application.interfaces.order.IAddCouponOrderUseCase;
+import com.lumiere.domain.entities.Order;
+import com.lumiere.domain.entities.User;
+import com.lumiere.domain.enums.StatusEnum.Status;
+import com.lumiere.domain.repositories.OrderRepository;
+import com.lumiere.domain.repositories.UserRepository;
+import com.lumiere.domain.services.OrderService;
+
+public class AddCouponOrderUseCase implements IAddCouponOrderUseCase {
+    private final UserRepository userRepo;
+    private final OrderRepository orderRepo;
+
+    protected AddCouponOrderUseCase(UserRepository userRepo, OrderRepository orderRepo) {
+        this.orderRepo = orderRepo;
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public AddCouponOutput execute(AddCouponInput input) {
+        User user = userRepo.findUserByAuthId(input.userId()).orElseThrow(UserNotFoundException::new);
+
+        Order order = orderRepo.findByUserIdAndStatus(user.getId(), Status.IN_PROGRESS)
+                .orElseThrow(OrderNotFoundException::new);
+
+        Order newOrder = OrderService.useCoupon(order, input.coupon().coupon());
+
+        orderRepo.update(newOrder);
+
+        return new AddCouponOutput();
+    }
+}
