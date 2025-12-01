8c16535da59733cb293cfcda32b3dfb086cd0875
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-12-01 15:23:41 -0300

    feat: coupon repository

---
+++ src/main/java/com/lumiere/domain/repositories/CouponRepository.java
@@ -0,0 +1,7 @@
+package com.lumiere.domain.repositories;
+
+import com.lumiere.domain.entities.Coupon;
+import com.lumiere.domain.repositories.base.BaseRepository;
+
+public interface CouponRepository extends BaseRepository<Coupon> {
+}
--- src/main/java/com/lumiere/infrastructure/config/serialization/JacksonConfig.java
+++ src/main/java/com/lumiere/infrastructure/config/serialization/JacksonConfig.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
-import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer; // Importante!
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
