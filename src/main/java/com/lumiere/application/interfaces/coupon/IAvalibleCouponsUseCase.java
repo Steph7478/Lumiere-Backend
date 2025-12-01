335e60e27c8ee010ace3f387912340a22231dfbf
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-12-01 17:53:08 -0300

    feat: avalible coupon interface

---
+++ src/main/java/com/lumiere/application/interfaces/coupon/IAvalibleCouponsUseCase.java
@@ -0,0 +1,9 @@
+package com.lumiere.application.interfaces.coupon;
+
+import com.lumiere.application.dtos.coupon.query.AvalibleCouponsInput;
+import com.lumiere.application.dtos.coupon.query.AvalibleCouponsOutput;
+import com.lumiere.application.interfaces.base.BaseUseCase;
+
+public interface IAvalibleCouponsUseCase extends BaseUseCase<AvalibleCouponsInput, AvalibleCouponsOutput> {
+
+}
