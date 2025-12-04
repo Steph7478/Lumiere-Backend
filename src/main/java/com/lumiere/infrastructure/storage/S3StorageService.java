4fab53f5b72f639a032df4c2d0e0417de8710136
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-12-04 15:20:39 -0300

    refactor: create bucket

--- src/main/java/com/lumiere/infrastructure/storage/S3StorageService.java
+++ src/main/java/com/lumiere/infrastructure/storage/S3StorageService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.*;
+
 import java.io.InputStream;
 
 @Service
@@ -15,8 +16,29 @@ public class S3StorageService {
         this.s3Client = s3Client;
     }
 
-    public String uploadFile(InputStream fileStream, long contentLength, String keyName, String contentType,
+    private void createBucketIfNotExists(String bucketName) {
+        boolean exists = s3Client
+                .listBuckets()
+                .buckets()
+                .stream()
+                .anyMatch(b -> b.name().equals(bucketName));
+
+        if (!exists) {
+            s3Client.createBucket(
+                    CreateBucketRequest.builder()
+                            .bucket(bucketName)
+                            .build());
+        }
+    }
+
+    public String uploadFile(
+            InputStream fileStream,
+            long contentLength,
+            String keyName,
+            String contentType,
             String bucketName) {
+
+        createBucketIfNotExists(bucketName);
 
         PutObjectRequest putObjectRequest = PutObjectRequest.builder()
                 .bucket(bucketName)
@@ -25,7 +47,8 @@ public class S3StorageService {
                 .contentType(contentType)
                 .build();
 
-        PutObjectResponse response = s3Client.putObject(putObjectRequest,
+        PutObjectResponse response = s3Client.putObject(
+                putObjectRequest,
                 RequestBody.fromInputStream(fileStream, contentLength));
 
         return response.eTag();
