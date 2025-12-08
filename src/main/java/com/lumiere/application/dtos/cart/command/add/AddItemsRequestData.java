7f51c93187a5bfb028b96a51747a2630f4c9400c
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-12-08 18:15:47 -0300

    feat: validation additems

--- src/main/java/com/lumiere/application/dtos/cart/command/add/AddItemsRequestData.java
+++ src/main/java/com/lumiere/application/dtos/cart/command/add/AddItemsRequestData.java
@@ -3,7 +3,9 @@ package com.lumiere.application.dtos.cart.command.add;
 import java.util.List;
 import java.util.UUID;
 
+import jakarta.validation.constraints.*;
+
 public record AddItemsRequestData(List<InnerAddItemsRequestData> items) {
-    public record InnerAddItemsRequestData(UUID productId, int quantity) {
+    public record InnerAddItemsRequestData(@NotNull UUID productId, @Min(1) @Max(100) int quantity) {
     }
 }
