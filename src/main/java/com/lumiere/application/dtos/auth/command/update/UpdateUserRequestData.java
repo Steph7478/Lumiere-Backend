56acf57c0dedeea8625ebb9852bf266793acec8b
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-12-08 18:10:07 -0300

    feat: size dto validation name

--- src/main/java/com/lumiere/application/dtos/auth/command/update/UpdateUserRequestData.java
+++ src/main/java/com/lumiere/application/dtos/auth/command/update/UpdateUserRequestData.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 import jakarta.validation.constraints.*;
 
 public record UpdateUserRequestData(
-                @NotBlank Optional<String> name,
+                @NotBlank @Size(min = 3, max = 50, message = "Name must have between 3 to 50 characters") Optional<String> name,
                 @NotBlank @Email Optional<String> email,
                 @NotBlank @Size(min = 8, message = "Password must be at least 8 characters long") @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*.])(?=.{8,}).*$", message = "Password must be at least 8 characters long and contain letters, numbers, and a special character (e.g., !@#$%^&*.).") Optional<String> password) {
 
