30f744cccee9d6de8fc86b4f7d8b2af3a0c57154
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-12-08 18:08:40 -0300

    feat: dto validation request data

--- src/main/java/com/lumiere/application/dtos/auth/command/update/UpdateUserRequestData.java
+++ src/main/java/com/lumiere/application/dtos/auth/command/update/UpdateUserRequestData.java
@@ -2,10 +2,12 @@ package com.lumiere.application.dtos.auth.command.update;
 
 import java.util.Optional;
 
+import jakarta.validation.constraints.*;
+
 public record UpdateUserRequestData(
-                Optional<String> name,
-                Optional<String> email,
-                Optional<String> password) {
+                @NotBlank Optional<String> name,
+                @NotBlank @Email Optional<String> email,
+                @NotBlank @Size(min = 8, message = "Password must be at least 8 characters long") @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*.])(?=.{8,}).*$", message = "Password must be at least 8 characters long and contain letters, numbers, and a special character (e.g., !@#$%^&*.).") Optional<String> password) {
 
         public boolean isCompleteUpdate() {
                 return name.isPresent() &&
