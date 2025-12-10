d8c783a866a98122ae9e04154905dac95975b263
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-12-10 11:19:09 -0300

    feat: validation cart output

--- src/main/java/com/lumiere/application/dtos/cart/command/add/AddCartOuput.java
+++ src/main/java/com/lumiere/application/dtos/cart/command/add/AddCartOuput.java
@@ -2,6 +2,8 @@ package com.lumiere.application.dtos.cart.command.add;
 
 import com.lumiere.domain.readmodels.CartReadModel;
 
+import jakarta.validation.constraints.*;
+
 public record AddCartOuput(
-                CartReadModel cart) {
+        @NotNull CartReadModel cart) {
 }
