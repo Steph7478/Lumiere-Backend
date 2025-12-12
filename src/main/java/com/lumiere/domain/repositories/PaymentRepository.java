5fc05535853884d41fdc863a7868349835b181e6
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-12-12 14:44:22 -0300

    feat: payment repo

---
+++ src/main/java/com/lumiere/domain/repositories/PaymentRepository.java
@@ -0,0 +1,8 @@
+package com.lumiere.domain.repositories;
+
+import com.lumiere.domain.entities.Payment;
+import com.lumiere.domain.repositories.base.BaseRepository;
+
+public interface PaymentRepository extends BaseRepository<Payment> {
+
+}
