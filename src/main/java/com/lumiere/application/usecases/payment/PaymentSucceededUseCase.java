513cb0095241d5fd3172370e694e4fc500072ac8
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-12-13 12:43:08 -0300

    feat: payment succeded usecase

---
+++ src/main/java/com/lumiere/application/usecases/payment/PaymentSucceededUseCase.java
@@ -0,0 +1,66 @@
+package com.lumiere.application.usecases.payment;
+
+import com.lumiere.application.exceptions.order.OrderNotFoundException;
+import com.lumiere.application.webhooks.WebhookEvent;
+import com.lumiere.domain.entities.Order;
+import com.lumiere.domain.entities.Payment;
+import com.lumiere.domain.enums.PaymentMethodEnum;
+import com.lumiere.domain.factories.PaymentFactory;
+import com.lumiere.domain.repositories.OrderRepository;
+import com.lumiere.domain.repositories.PaymentRepository;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentSucceededUseCase {
+
+    private final PaymentRepository paymentRepo;
+    private final OrderRepository orderRepo;
+
+    public PaymentSucceededUseCase(
+            PaymentRepository paymentRepo,
+            OrderRepository orderRepo) {
+        this.paymentRepo = paymentRepo;
+        this.orderRepo = orderRepo;
+    }
+
+    public void execute(WebhookEvent event) {
+        Map<String, Object> d = event.data();
+
+        UUID paymentId = UUID.fromString(d.get("paymentId").toString());
+
+        Order order = orderRepo.findById(
+                UUID.fromString(d.get("orderId").toString())).orElseThrow(OrderNotFoundException::new);
+
+        Object pm = d.get("paymentMethod");
+
+        if (!(pm instanceof List<?> list) || list.isEmpty()) {
+            throw new IllegalStateException("Invalid payment method");
+        }
+
+        PaymentMethodEnum method = switch (list.get(0).toString()) {
+            case "pix" -> PaymentMethodEnum.PIX;
+            case "boleto" -> PaymentMethodEnum.BOLETO;
+            case "card" -> PaymentMethodEnum.CREDIT_CARD;
+            default -> throw new IllegalStateException("Unsupported payment method");
+        };
+
+        LocalDateTime paidAt = Instant
+                .ofEpochSecond(((Number) d.get("createdAt")).longValue())
+                .atZone(ZoneId.systemDefault())
+                .toLocalDateTime();
+
+        Order paidOrder = order.markAsPaid(paymentId);
+        Payment payment = PaymentFactory.create(paymentId, method, paidAt);
+
+        orderRepo.save(paidOrder);
+        paymentRepo.save(payment);
+    }
+}
