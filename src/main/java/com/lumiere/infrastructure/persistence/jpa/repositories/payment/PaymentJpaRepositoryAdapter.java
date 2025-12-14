fe151344d6178ffada46bdfdaa534c9f658eaab4
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-12-14 11:46:37 -0300

    feat: implements repository

--- src/main/java/com/lumiere/infrastructure/persistence/jpa/repositories/payment/PaymentJpaRepositoryAdapter.java
+++ src/main/java/com/lumiere/infrastructure/persistence/jpa/repositories/payment/PaymentJpaRepositoryAdapter.java
@@ -3,6 +3,7 @@ package com.lumiere.infrastructure.persistence.jpa.repositories.payment;
 import org.springframework.stereotype.Repository;
 
 import com.lumiere.domain.entities.Payment;
+import com.lumiere.domain.repositories.PaymentRepository;
 import com.lumiere.infrastructure.mappers.PaymentMapper;
 import com.lumiere.infrastructure.persistence.jpa.entities.PaymentJpaEntity;
 import com.lumiere.infrastructure.persistence.jpa.repositories.base.BaseRepositoryAdapter;
@@ -10,7 +11,8 @@ import com.lumiere.infrastructure.persistence.jpa.repositories.base.BaseReposito
 import jakarta.persistence.EntityManager;
 
 @Repository
-public class PaymentJpaRepositoryAdapter extends BaseRepositoryAdapter<Payment, PaymentJpaEntity> {
+public class PaymentJpaRepositoryAdapter extends BaseRepositoryAdapter<Payment, PaymentJpaEntity>
+        implements PaymentRepository {
 
     protected PaymentJpaRepositoryAdapter(PaymentJpaRepository jpaRepository,
             PaymentMapper mapper, EntityManager entityManager) {
