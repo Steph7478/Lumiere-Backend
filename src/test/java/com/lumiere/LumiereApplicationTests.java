51944daf7182c0597fbb82ff0f18ee156c3c6239
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-12-31 13:17:31 -0300

    feat: mockito beans

--- src/test/java/com/lumiere/LumiereApplicationTests.java
+++ src/test/java/com/lumiere/LumiereApplicationTests.java
@@ -1,12 +1,24 @@
 package com.lumiere;
 
 import org.junit.jupiter.api.Test;
+import org.redisson.api.RedissonClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import com.lumiere.infrastructure.config.environment.DotenvLoader;
+import com.lumiere.infrastructure.stripe.config.StripeConfig;
+import com.lumiere.infrastructure.stripe.services.StripeEventConstructorService;
 
 @SpringBootTest
 class LumiereApplicationTests {
+	@MockitoBean
+	private StripeConfig stripeConfig;
+
+	@MockitoBean
+	private StripeEventConstructorService stripeEventConstructorService;
+
+	@MockitoBean
+	private RedissonClient redissonClient;
 
 	@Test
 	void contextLoads() {
